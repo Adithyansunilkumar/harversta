3eb1b23f1a93aeaf37e1493237b42c40e48b65e9
Adithyan <skradithyan@gmail.com>
2026-01-17 11:13:49 +0530

    mongodb connected

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cors from "cors";
+import connectDB from "./config/db.js";
 dotenv.config();
 
 const app = express();
 
-app.get("/",(req,res)=>{
-   res.send("Server is ready")
-})
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+   res.send("Server is running");
+});
+
+app.post("/api/test", (req, res) => {
+   const { name } = req.body;
+   res.status(201).json({
+      message: "request recieved",
+      name,
+   });
+});
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => {
-   console.log(`Server started at http://localhost:${PORT}`);
-});
+
+const startServer = async () => {
+   await connectDB()
+   app.listen(PORT,()=>{
+      console.log(`Server started at http://localhost:${PORT}`)
+   })
+};
+
+startServer()
